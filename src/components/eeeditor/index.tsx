8c9ad95fbeddab05f938648416f3abed7c11df47
bj75326 <superice_gy@163.com>
2020-12-29 12:52:11 +0800

    ⛲️ start

---
+++ src/components/eeeditor/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Editor from '@draft-js-plugins/editor';
+import { EditorState, convertToRaw } from 'draft-js';
+import classNames from 'classnames';
+import {} from 'antd';
+
+export interface EEEditor {}
+
+const EEEditor: React.FC<EEEditor> = (props) => {
+  return <div></div>;
+};
