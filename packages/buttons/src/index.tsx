644e5014952c13b54b43102962f284d216ae80c3
bj75326 <superice_gy@163.com>
2021-02-06 01:27:00 +0800

    📡 buttons

--- packages/buttons/src/index.tsx
+++ packages/buttons/src/index.tsx
@@ -1,3 +1,25 @@
-import { } from 'react';
+import {ComponentType } from 'react';
+import { EditorState } from 'draft-js';
+import 
 
-export interface 
+export interface Locale { 
+
+}
+
+export interface EEEditorButtonProps {
+  prefixCls?: string;
+  className?: string;
+  style?: string;
+  locale?: Locale;
+}
+
+export interface EEEditorStyleButtonProps extends EEEditorButtonProps {
+  getEditorState (): EditorState;
+  setEditorState (editorState: EditorState): void;
+}
+
+export type EEEditorStyleButtonType = ComponentType<EEEditorStyleButtonProps>;
+
+export {
+
+};
