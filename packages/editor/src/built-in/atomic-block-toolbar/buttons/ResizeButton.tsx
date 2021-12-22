1c410ca7e32da535c2e0b0d162a0d256a209da0b
bj75326 <superice_gy@163.com>
2021-12-22 17:33:05 +0800

    🧀 resizer box

--- packages/editor/src/built-in/atomic-block-toolbar/buttons/ResizeButton.tsx
+++ packages/editor/src/built-in/atomic-block-toolbar/buttons/ResizeButton.tsx
@@ -6,7 +6,12 @@ import React, {
   MouseEvent,
 } from 'react';
 import { createPortal } from 'react-dom';
-import { resizeIcon, PluginMethods, EEEditorContext } from '../../..';
+import {
+  resizeIcon,
+  PluginMethods,
+  EEEditorContext,
+  getEditorRootDomNode,
+} from '../../..';
 import lang, { zhCN, Locale, Languages } from '../../../locale';
 import { AtomicBlockProps } from '..';
 import { Tooltip } from 'antd';
@@ -46,7 +51,7 @@ const ResizeButtonComponent: React.FC<
     getBlockProps,
   } = props;
 
-  const { getProps } = pluginMethods;
+  const { getProps, getEditorRef } = pluginMethods;
 
   let locale: Locale = zhCN;
   if (getProps && languages) {
@@ -60,12 +65,23 @@ const ResizeButtonComponent: React.FC<
   const [active, setActive] = useState<boolean>(false);
 
   const handleBtnClick = (e: MouseEvent): void => {
+    console.log('test ', getEditorRef());
+
     if (btnKey) {
       const newActiveBtn = activeBtn === btnKey ? '' : btnKey;
       changeActiveBtn(newActiveBtn);
     } else {
       setActive(!active);
     }
+  };
+
+  const getContainer = () => {
+    if (getEditorRef()) {
+      return getEditorRootDomNode(getEditorRef()).ownerDocument.querySelector(
+        `figure[data-offset-key="${getBlockProps().offsetKey}"]`,
+      );
+    }
+    return null;
   };
 
   const getTipTitle = (name: string): ReactNode => (
@@ -91,7 +107,25 @@ const ResizeButtonComponent: React.FC<
           {resizeIcon}
         </span>
       </Tooltip>
-      {}
+      {(btnKey ? activeBtn === btnKey : active) &&
+        getContainer() &&
+        createPortal(
+          <>
+            <span
+              className={`${prefixCls}-resize-handler ${prefixCls}-resizer-tl`}
+            ></span>
+            <span
+              className={`${prefixCls}-resize-handler ${prefixCls}-resizer-tr`}
+            ></span>
+            <span
+              className={`${prefixCls}-resize-handler ${prefixCls}-resizer-br`}
+            ></span>
+            <span
+              className={`${prefixCls}-resize-handler ${prefixCls}-resizer-bl`}
+            ></span>
+          </>,
+          getContainer(),
+        )}
     </>
   );
 };
